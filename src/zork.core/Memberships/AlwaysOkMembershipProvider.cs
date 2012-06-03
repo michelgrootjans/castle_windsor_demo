a86d4b3cdf6ed595f4c21bd150749883c74ccea6
Michel Grootjans <michel.grootjans@gmail.com>
2012-06-03 17:22:46 +0200

    cleaned up the account_controller

--- src/zork.core/Memberships/AlwaysOkMembershipProvider.cs
+++ src/zork.core/Memberships/AlwaysOkMembershipProvider.cs
@@ -2,13 +2,23 @@ namespace zork.core.Memberships
 {
     public interface IMembershipProvider
     {
-        bool ValidateUser(string userName, string password);
+        bool IsValidUser(string userName, string password);
+        void RegisterNewUser(string userName, string password, string email);
+        void ChangePassword(string userName, string oldPassword, string newPassword);
     }
     public class AlwaysOkMembershipProvider : IMembershipProvider
     {
-        public bool ValidateUser(string userName, string password)
+        public bool IsValidUser(string userName, string password)
         {
             return true;
+        }
+
+        public void RegisterNewUser(string userName, string password, string email)
+        {
+        }
+
+        public void ChangePassword(string userName, string oldPassword, string newPassword)
+        {
         }
     }
 }
--- src/zork.web/Controllers/AccountController.cs
+++ src/zork.web/Controllers/AccountController.cs
@@ -27,27 +27,18 @@ namespace zork.web.Controllers
         [HttpPost]
         public ActionResult LogOn(LogOnModel model, string returnUrl)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid) return View(model);
+
+            if (membershipProvider.IsValidUser(model.UserName, model.Password))
             {
-                if (membershipProvider.ValidateUser(model.UserName, model.Password))
-                {
-                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
-                    if (IsRedirectable(returnUrl))
-                    {
-                        return Redirect(returnUrl);
-                    }
-                    else
-                    {
-                        return RedirectToAction("Index", "Home");
-                    }
-                }
-                else
-                {
-                    ModelState.AddModelError("", "The user name or password provided is incorrect.");
-                }
+                FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+                if (IsRedirectable(returnUrl)) 
+                    return Redirect(returnUrl);
+
+                return RedirectToAction("Index", "Home");
             }
 
-            // If we got this far, something failed, redisplay form
+            ModelState.AddModelError("", "The user name or password provided is incorrect.");
             return View(model);
         }
 
@@ -55,7 +46,6 @@ namespace zork.web.Controllers
         public ActionResult LogOff()
         {
             FormsAuthentication.SignOut();
-
             return RedirectToAction("Index", "Home");
         }
 
@@ -67,25 +57,20 @@ namespace zork.web.Controllers
         [HttpPost]
         public ActionResult Register(RegisterModel model)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid) return View(model);
+
+            try
             {
-                // Attempt to register the user
-                MembershipCreateStatus createStatus;
-                Membership.CreateUser(model.UserName, model.Password, model.Email, null, null, true, null, out createStatus);
-
-                if (createStatus == MembershipCreateStatus.Success)
-                {
-                    FormsAuthentication.SetAuthCookie(model.UserName, false /* createPersistentCookie */);
-                    return RedirectToAction("Index", "Home");
-                }
-                else
-                {
-                    ModelState.AddModelError("", ErrorCodeToString(createStatus));
-                }
+                membershipProvider.RegisterNewUser(model.UserName, model.Password, model.Email);
+                FormsAuthentication.SetAuthCookie(model.UserName, false /* createPersistentCookie */);
+                return RedirectToAction("Index", "Home");
             }
-
-            // If we got this far, something failed, redisplay form
-            return View(model);
+            catch (Exception e)
+            {
+                ModelState.AddModelError("", e.Message);
+                return View(model);
+            }
+            
         }
 
         [Authorize]
@@ -98,75 +83,23 @@ namespace zork.web.Controllers
         [HttpPost]
         public ActionResult ChangePassword(ChangePasswordModel model)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid) return View(model);
+
+            try
             {
-
-                // ChangePassword will throw an exception rather
-                // than return false in certain failure scenarios.
-                bool changePasswordSucceeded;
-                try
-                {
-                    MembershipUser currentUser = Membership.GetUser(User.Identity.Name, true /* userIsOnline */);
-                    changePasswordSucceeded = currentUser.ChangePassword(model.OldPassword, model.NewPassword);
-                }
-                catch (Exception)
-                {
-                    changePasswordSucceeded = false;
-                }
-
-                if (changePasswordSucceeded)
-                {
-                    return RedirectToAction("ChangePasswordSuccess");
-                }
-                else
-                {
-                    ModelState.AddModelError("", "The current password is incorrect or the new password is invalid.");
-                }
+                membershipProvider.ChangePassword(User.Identity.Name, model.OldPassword, model.NewPassword);
+                return RedirectToAction("ChangePasswordSuccess");
             }
-
-            // If we got this far, something failed, redisplay form
-            return View(model);
+            catch
+            {
+                ModelState.AddModelError("", "The current password is incorrect or the new password is invalid.");
+                return View(model);
+            }
         }
 
         public ActionResult ChangePasswordSuccess()
         {
             return View();
-        }
-
-        private static string ErrorCodeToString(MembershipCreateStatus createStatus)
-        {
-            switch (createStatus)
-            {
-                case MembershipCreateStatus.DuplicateUserName:
-                    return "User name already exists. Please enter a different user name.";
-
-                case MembershipCreateStatus.DuplicateEmail:
-                    return "A user name for that e-mail address already exists. Please enter a different e-mail address.";
-
-                case MembershipCreateStatus.InvalidPassword:
-                    return "The password provided is invalid. Please enter a valid password value.";
-
-                case MembershipCreateStatus.InvalidEmail:
-                    return "The e-mail address provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.InvalidAnswer:
-                    return "The password retrieval answer provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.InvalidQuestion:
-                    return "The password retrieval question provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.InvalidUserName:
-                    return "The user name provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.ProviderError:
-                    return "The authentication provider returned an error. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
-
-                case MembershipCreateStatus.UserRejected:
-                    return "The user creation request has been canceled. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
-
-                default:
-                    return "An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
-            }
         }
 
         private bool IsRedirectable(string returnUrl)
