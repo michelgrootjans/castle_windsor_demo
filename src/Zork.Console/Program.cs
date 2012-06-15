6e1902ae0f5ce33b8853e602ee75cd117765b075
Michel Grootjans <michel.grootjans@gmail.com>
2012-06-15 17:36:36 +0200

    cleanup

--- src/Zork.Console/Program.cs
+++ src/Zork.Console/Program.cs
@@ -1,52 +1,20 @@
 ﻿using System;
-using Castle.Windsor;
-using Zork.ConsoleApp.Utilities;
-using Zork.Core.Memberships;
 
 namespace Zork.ConsoleApp
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            var game = new Game();
-            game.Run();
+            try
+            {
+                new Game().Run();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
             Console.ReadLine();
         }
-    }
-
-    internal class Game
-    {
-        private readonly IWindsorContainer container;
-
-        public Game()
-        {
-            container = BootStrapper.GetContainer();
-        }
-
-        public void Run()
-        {
-            PrintTitle();
-            Login();
-        }
-
-        private void PrintTitle()
-        {
-            Console.WriteLine("Welcome to Zork");
-            Console.WriteLine("---------------");
-        }
-
-        private void Login()
-        {
-            Console.WriteLine("Please login");
-            Console.Write("Username: ");
-            var username = Console.ReadLine();
-            Console.Write("Password: ");
-            var password = ConsolePasswordReader.ReadLine();
-
-            var provider = container.Resolve<IMembershipProvider>();
-            provider.IsValidUser(username, password);
-        }
-
     }
 }
